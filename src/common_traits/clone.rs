d4b1f6f62fbbf82b31fadc6f977a732a866d5c45
orxfun <orx.ugur.arikan@gmail.com>
2024-09-22 17:33:55 +0200

    Clone is implemented

--- src/common_traits/clone.rs
+++ src/common_traits/clone.rs
@@ -1,14 +1,22 @@
-// use crate::ConcurrentVec;
-// use orx_concurrent_option::ConcurrentOption;
-// use orx_fixed_vec::IntoConcurrentPinnedVec;
+use crate::ConcurrentVec;
 
-// impl<T, P> Clone for ConcurrentVec<T, P>
-// where
-//     P: IntoConcurrentPinnedVec<ConcurrentOption<T>>,
-//     T: Clone,
-// {
-//     fn clone(&self) -> Self {
-//         let core = unsafe { self.core.clone_with_len(self.len()) };
-//         Self { core }
-//     }
-// }
+impl<T> Clone for ConcurrentVec<T>
+where
+    T: Clone,
+{
+    /// A thread-safe method to clone the concurrent vec.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use orx_concurrent_vec::*;
+    ///
+    /// let vec: ConcurrentVec<_> = (0..4).into_iter().collect();
+    /// let clone = vec.clone();
+    ///
+    /// assert_eq!(&clone, &[0, 1, 2, 3]);
+    /// ```
+    fn clone(&self) -> Self {
+        self.iter_cloned().collect()
+    }
+}
